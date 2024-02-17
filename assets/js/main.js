cee22491870945b65fae9cfdb8ecbc0f87653fc8
AleksejPlay <alekej.makedon@gmail.com>
2024-02-17 19:32:36 +0200

    README file removed, added site structure

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+function MoveIn(el){
+    let elPos = el.offsetLeft;
+    let elWidth = el.offsetWidth;
+    var box = document.getElementById("box");
+    
+    box.style.left = elPos.toString().concat("px");
+    box.style.width = elWidth.toString().concat("px");
+}
+function MoveOut(){
+    var actElPos = document.getElementById("active").offsetLeft;
+    let actElWidth = document.getElementById("active").offsetWidth;
+    var box = document.getElementById("box");
+
+    box.style.left = actElPos.toString().concat("px");
+    box.style.width = actElWidth.toString().concat("px");
+}
+
+function ChangeID(newActive){
+    lastActive = document.getElementById("active");
+    lastActive.removeAttribute('id');
+
+    newActive.setAttribute("id" , "active");
+
+    MoveOut();
+}
